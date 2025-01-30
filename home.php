f64b458b12ff67a582f45bb860657aca96cf6d28
bernishh <baoueleonora96@gmail.com>
2025-01-30 21:41:09 +0100

    ajout des pages

---
+++ home.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="home.css">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar">
+    <ul>
+        <li><a href="home.php">Home</a></li>
+        <li><a href="menu.php">Menu</a></li>
+        <li><a href="reservation.php">Reservation</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="catalog.php">Catalog</a></li>
+        <li><a href="contact.php">Contact</a></li>
+    </ul>
+</nav>
+    <section class="hero">
+        <h1>Bienvenue chez MAMA AFRICA</h1>
+        <p>Frais, authentique, 100% africain.</p>
+        <a href="catalog.php" class="btn">Voir le menu</a>
+      </section>
+      <section class="popular">
+        <h2>Repas populaires</h2>
+      </section>
+      
+      
+</body>
+</html>
